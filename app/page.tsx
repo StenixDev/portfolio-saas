966119816351aa700ebeb5dabf6ea1b481442377
StenixDev <stenix.dev@gmail.com>
2025-12-11 21:09:16 +0800

    shadcn ui installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <Button size='lg'>Click Me</Button>
+    </div>
+  );
 }
 export default Home;
